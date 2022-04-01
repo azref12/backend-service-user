dac98b8824538219d627f4567a32f30cd7b90d45
asus <arefani13@.com>
2022-04-01 14:25:19 +0700

    adding class in auth > serializers.py & auth > urls.py

--- auths/urls.py
+++ auths/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from .views import *
-from auths.views import MyObtainTokenPairView, RegisterView
+from auths.views import MyObtainTokenPairView, RegisterView, ChangePasswordSerializer, LogoutView
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path ('login/', MyObtainTokenPairView.as_view(), name='token_obtain_pair'),
     path ('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    # path ('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path ('register/', RegisterView.as_view(), name='auth_register'),
+    path ('change_password/<int:pk>/', ChangePasswordView.as_view(), name='auth_change_password'),
+    path ('update_profile/<int:pk>/', UpdateProfileView.as_view(), name='auth_update_profile'),
+    path ('logout/', LogoutView.as_view(), name='auth_logout')
 ]
--- auths/views.py
+++ auths/views.py
@@ -2,8 +2,12 @@ from .serializers import MyTokenObtainPairSerializer
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework_simplejwt.views import TokenObtainPairView
 from django.contrib.auth.models import User
-from .serializers import RegisterSerializer
+from .serializers import RegisterSerializer, ChangePasswordSerializer, UpdateUserSerializer
 from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.response import Response
+from rest_framework import status
 
 class MyObtainTokenPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
@@ -13,3 +17,26 @@ class RegisterView (generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
     permission_classes = (AllowAny,)
+
+class ChangePasswordView(generics.UpdateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (IsAuthenticated,)
+    serializer_class = ChangePasswordSerializer
+
+class UpdateProfileView(generics.UpdateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (IsAuthenticated,)
+    serializer_class = UpdateUserSerializer
+
+class LogoutView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh_token"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
